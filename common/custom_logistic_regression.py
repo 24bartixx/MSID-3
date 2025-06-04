a89e38f79afaf5957da1372af98e3f577ed33e83
Bartosh <jestgitbartix@gmail.com>
2025-06-04 03:05:36 +0200

    refactor: prepare logistic regression for convergence diagram

--- common/custom_logistic_regression.py
+++ common/custom_logistic_regression.py
@@ -31,76 +31,115 @@ class CustomLogisticRegressionBinary(BaseEstimator, RegressorMixin):
     def predict(self, X):
         return (self.predict_proba(X) >= 0.5).astype(int)
     
-    def _fit_base(self, X, y):
+    def fit_init(self, X, y):
         if isinstance(X, pd.DataFrame):
             X = X.values  
         if scipy.sparse.issparse(X):
             X = X.toarray()    
         
-        X_matrix = np.c_[np.ones(shape=(X.shape[0],1)), X]
+        self.X_matrix = np.c_[np.ones(shape=(X.shape[0],1)), X]
             
         if isinstance(y, pd.Series):
             y = y.values
-        y_matrix = y.reshape(-1, 1)
+        self.y_matrix = y.reshape(-1, 1)
             
-        return X_matrix, y_matrix
-    
-    
-    def fit(self, X, y):
-        X_matrix, y_matrix = self._fit_base(X, y)
-        
-        samples_count, feat_count = X_matrix.shape
+        self.samples_count, feat_count = self.X_matrix.shape
         self.coefficients_ = np.zeros((feat_count, 1))
-
-        for _ in range(self.epochs):
             
-            if self.mini_batch and self.batch_size:           # mini batch gradient descent
-                indices = np.random.permutation(samples_count)
-                for i in range(0, samples_count, self.batch_size):
-                    batch_indices = indices[i:i+self.batch_size]
-                    X_batch = X_matrix[batch_indices]
-                    y_batch = y_matrix[batch_indices]
-                    
-                    sigmoid = 1 / (1 + np.exp(-X_batch @ self.coefficients_))
-                    gradients = X_batch.T @ (sigmoid - y_batch) / len(X_batch)
-                    
-                    l2_term = (self.C / len(X_batch)) *self.coefficients_
-                    l2_term[0] = 0
-                    
-                    gradients += l2_term
-                    self.coefficients_ -= self.learning_rate * gradients
-            else:
+        # return X_matrix, y_matrix
+    
+    
+    def fit_epoch(self):
+        if self.mini_batch and self.batch_size:           # mini batch gradient descent
+            indices = np.random.permutation(self.samples_count)
+            for i in range(0, self.samples_count, self.batch_size):
+                batch_indices = indices[i:i+self.batch_size]
+                X_batch = self.X_matrix[batch_indices]
+                y_batch = self.y_matrix[batch_indices]
                 
-                if self.batch_size:                         # stochastic batch gradient descent
-                    indices = np.random.choice(samples_count, self.batch_size, replace = False)
-                    X_batch = X_matrix[indices]
-                    y_batch = y_matrix[indices]
-                    factor = 1 / self.batch_size
-                else:                                       # simple batch gradient descent
-                    X_batch = X_matrix
-                    y_batch = y_matrix
-                    factor = 1 / samples_count
-                    
                 sigmoid = 1 / (1 + np.exp(-X_batch @ self.coefficients_))
-                gradients = factor * X_batch.T @ (sigmoid - y_batch)
+                gradients = X_batch.T @ (sigmoid - y_batch) / len(X_batch)
                 
                 l2_term = (self.C / len(X_batch)) *self.coefficients_
                 l2_term[0] = 0
                 
                 gradients += l2_term
                 self.coefficients_ -= self.learning_rate * gradients
+        else:
+            
+            if self.batch_size:                         # stochastic batch gradient descent
+                indices = np.random.choice(self.samples_count, self.batch_size, replace = False)
+                X_batch = self.X_matrix[indices]
+                y_batch = self.y_matrix[indices]
+                factor = 1 / self.batch_size
+            else:                                       # simple batch gradient descent
+                X_batch = self.X_matrix
+                y_batch = self.y_matrix
+                factor = 1 / self.samples_count
+                
+            sigmoid = 1 / (1 + np.exp(-X_batch @ self.coefficients_))
+            gradients = factor * X_batch.T @ (sigmoid - y_batch)
+            
+            l2_term = (self.C / len(X_batch)) *self.coefficients_
+            l2_term[0] = 0
+            
+            gradients += l2_term
+            self.coefficients_ -= self.learning_rate * gradients
+    
+    
+    def fit(self, X, y):
+        self.fit_init(X, y)
+        for _ in range(self.epochs):
+            self.fit_epoch()
+            
+            # if self.mini_batch and self.batch_size:           # mini batch gradient descent
+            #     indices = np.random.permutation(samples_count)
+            #     for i in range(0, samples_count, self.batch_size):
+            #         batch_indices = indices[i:i+self.batch_size]
+            #         X_batch = X_matrix[batch_indices]
+            #         y_batch = y_matrix[batch_indices]
+                    
+            #         sigmoid = 1 / (1 + np.exp(-X_batch @ self.coefficients_))
+            #         gradients = X_batch.T @ (sigmoid - y_batch) / len(X_batch)
+                    
+            #         l2_term = (self.C / len(X_batch)) *self.coefficients_
+            #         l2_term[0] = 0
+                    
+            #         gradients += l2_term
+            #         self.coefficients_ -= self.learning_rate * gradients
+            # else:
+                
+            #     if self.batch_size:                         # stochastic batch gradient descent
+            #         indices = np.random.choice(samples_count, self.batch_size, replace = False)
+            #         X_batch = X_matrix[indices]
+            #         y_batch = y_matrix[indices]
+            #         factor = 1 / self.batch_size
+            #     else:                                       # simple batch gradient descent
+            #         X_batch = X_matrix
+            #         y_batch = y_matrix
+            #         factor = 1 / samples_count
+                    
+            #     sigmoid = 1 / (1 + np.exp(-X_batch @ self.coefficients_))
+            #     gradients = factor * X_batch.T @ (sigmoid - y_batch)
+                
+            #     l2_term = (self.C / len(X_batch)) *self.coefficients_
+            #     l2_term[0] = 0
+                
+            #     gradients += l2_term
+            #     self.coefficients_ -= self.learning_rate * gradients
 
     def transform(self, X):
             return self.predict(X)
     
     
 class CustomLogisticRegressionMulticlass(BaseEstimator, RegressorMixin):
-    def __init__(self, epochs=500, batch_size=None, learning_rate=0.01, C=0, mini_batch=True):
+    def __init__(self, epochs=500, batch_size=None, learning_rate=0.01, C=0, mini_batch=True, gete_convergence_diagram=False):
         self.epochs = epochs
         self.batch_size = batch_size
         self.learning_rate = learning_rate
         self.C = C
         self.mini_batch = mini_batch
+        self.get_convergence_diagram = gete_convergence_diagram
         self.binary_models = {}
         self.class_labels = None
         
@@ -116,8 +155,17 @@ class CustomLogisticRegressionMulticlass(BaseEstimator, RegressorMixin):
                 C=self.C,
                 mini_batch=self.mini_batch
             ))
-            binary_model.fit(X, y_bin)
             self.binary_models[class_label] = binary_model
+            if self.get_convergence_diagram:
+                binary_model.fit_init(X, y_bin)
+            else:
+                binary_model.fit(X, y_bin)
+            
+        if self.get_convergence_diagram:
+            for _ in range(self.epochs):
+                for binary_model in self.binary_models.values():
+                    binary_model.fit_epoch()
+        
         return self
     
     def predict_proba(self, X):
